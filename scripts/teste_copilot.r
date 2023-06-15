39f10dc953a8ffb13c0e49571c1ce200ab11668f
washington_silva <wssaleph@gmail.com>
2023-06-15 15:01:23 -0300

    reorganizacao finalizada

--- scripts/teste_copilot.r
+++ scripts/teste_copilot.r
@@ -1,3 +1,13 @@
 
 # teste do GitHub copilot
 library(tidyverse)
+
+# simular dados de um modelo de regressão linear múltipla com 3 variáveis explicativas
+set.seed(123)
+n <- 100
+x1 <- rnorm(n)
+x2 <- rnorm(n)
+x3 <- rnorm(n)
+y <- 1 + 2*x1 + 3*x2 + 4*x3 + rnorm(n)
+dados <- data.frame(y, x1, x2, x3)
+
