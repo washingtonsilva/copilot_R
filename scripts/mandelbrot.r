4fb03ff29746536b0d74e284c68fca7232b135bf
washington_silva <wssaleph@gmail.com>
2023-06-26 17:22:02 -0300

    tentativa em paralelo

--- scripts/mandelbrot.r
+++ scripts/mandelbrot.r
@@ -1,9 +1,19 @@
 
-# criar uma imagem de um fractal de Mandelbrot
+# iterar sobre as coordenadas complexas
+# for (i in 1:100) {
+#  z <- z^2 + m
+#   m <- m + (abs(z) < 2)
+# }
+
+# simular um fractal de Mandelbrot e salvar imagem
+
+# pacotes utilzados
+library(foreach)
+library(doParallel)
 library(ggplot2)
 
 # definir a resolução da imagem
-resolucao <- 1000
+resolucao <- 10000
 
 # criar uma matriz de coordenadas complexas
 x <- seq(-2, 1, length.out = resolucao)
@@ -13,11 +23,21 @@ z <- outer(x, y, FUN = function(x, y) complex(real = x, imaginary = y))
 # criar uma matriz para armazenar o número de iterações
 m <- matrix(0, resolucao, resolucao)
 
-# iterar sobre as coordenadas complexas
-for (i in 1:100) {
+# definir o número de núcleos de CPU a serem usados
+num_cores <- detectCores()
+
+# registrar os núcleos de CPU para uso com o foreach
+cl <- makeCluster(num_cores)
+registerDoParallel(cl)
+
+# iterar sobre as coordenadas complexas em paralelo
+m <- foreach(i = 1:100, .combine = "+") %dopar% {
   z <- z^2 + m
-  m <- m + (abs(z) < 2)
+  (abs(z) < 2)
 }
+
+# fechar a conexão com os núcleos de CPU
+stopCluster(cl)
 
 # criar um data frame com as coordenadas e o número de iterações
 df <- data.frame(x = rep(x, each = resolucao),
